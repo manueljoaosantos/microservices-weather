8a1e06471b9a17f6d3b941627d2d92044e26cbdf
Manuel Santos <manueljoaosantos@gmail.com>
2022-10-23 18:13:23 +0100

    Add initial deps

--- Weather.Precipitation/Program.cs
+++ Weather.Precipitation/Program.cs
@@ -1,5 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
+//http://localhost:4000/observation/zip
+app.MapGet("/observation/{zip}", (string zip, [FromQuery] int? days) => {
+    return Results.Ok(zip);
+});
 
 app.Run();
