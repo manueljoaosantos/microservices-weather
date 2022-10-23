021bf8db01662b03434078526ba53a292b945d90
Manuel Santos <manueljoaosantos@gmail.com>
2022-10-23 20:24:25 +0100

    Por hoje esta ok

--- ReadMe.txt
+++ ReadMe.txt
@@ -1,4 +1,4 @@
-﻿dotnet new sln
+dotnet new sln
 dotnet new console -o Weather.DataLoader
 dotnet new webapi -o Weather.Report
 dotnet new webapi -o Weather.Temperature
@@ -9,4 +9,11 @@ dotnet sln add .\Weather.Report\
 dotnet sln add .\Weather.Precipitation\
 
 dotnet build
+
+docker-compose up
+docker-compose up -d
+docker ps
+
+dotnet ef migrations add init-migration
+dotnet ef database update
 
---
+++ Weather.Precipitation/DataAccess/PrecipDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Weather.Precipitation.DataAccess
+{
+    public class PrecipDbContext : DbContext
+    {
+        //public PrecipDbContext(){ }
+        public PrecipDbContext(DbContextOptions<PrecipDbContext> opts) : base(opts) { }
+        public DbSet<Precipitation> Precipitation { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        { 
+            base.OnModelCreating(modelBuilder);
+            SnakeCaseIdentityTablesNames(modelBuilder);
+        }
+
+        private static void SnakeCaseIdentityTablesNames(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Precipitation>(b => { b.ToTable("precipitation"); });
+        }
+    }
+}
